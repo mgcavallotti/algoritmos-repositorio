750fadd3553d196191af4f76c37a39977223a633
Guadalupe <guadalupecavallotti@gmail.com>
2025-03-12 12:05:23 -0300

    agrego funciones

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,7 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+print("holaa")
+ def funcion():
+     print("chau")
+     
